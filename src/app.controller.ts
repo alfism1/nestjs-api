84f2f373d6f022c19e79bb6e920763a6e4264bf3
alfism1 <alfialfarisi@gmail.com>
2023-10-30 20:16:06 +0700

    e2e test config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './common/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   // inject the AppService into the AppController
   constructor(private readonly appService: AppService) {}
 
-  @Get('test')
+  @Get()
+  @Public()
   getHello(): string {
     // call the AppService's getHello method
     return this.appService.getHello();
