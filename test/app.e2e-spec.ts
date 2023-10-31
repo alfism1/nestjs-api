b2be802fdd0b09b0327650a41fdac7bc05e22006
alfism1 <alfialfarisi@gmail.com>
2023-11-01 06:20:15 +0700

    e2e config + env config + register/login e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,75 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { UsersModule } from 'src/modules/users/users.module';
+import { PostsModule } from 'src/modules/posts/posts.module';
+import { PrismaClient } from '@prisma/client';
 
-describe('AppController (e2e)', () => {
+describe('Superb API (e2e)', () => {
   let app: INestApplication;
+  let prismaClient: PrismaClient;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, UsersModule, PostsModule, PrismaClient],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
+
+    prismaClient = moduleFixture.get<PrismaClient>(PrismaClient);
+
+    await prismaClient.$executeRaw`TRUNCATE "public"."Post" RESTART IDENTITY CASCADE;`;
+    await prismaClient.$executeRaw`TRUNCATE "public"."User" RESTART IDENTITY CASCADE;`;
+  }, 30000);
+
+  afterAll(async () => {
+    await app.close();
+    await prismaClient.$disconnect();
+  }, 30000);
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  describe('Users', () => {
+    it('should create a user', async () => {
+      const user = {
+        email: 'newuser@e2e.test',
+        name: 'New User',
+        password: '12345678',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/users/register')
+        .send(user)
+        .expect(201);
+
+      expect(response.body).toEqual({
+        id: expect.any(Number),
+        email: user.email,
+        name: user.name,
+      });
+    });
+
+    it('should login a user', async () => {
+      const user = {
+        email: 'newuser@e2e.test',
+        password: '12345678',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/users/login')
+        .send(user)
+
+        .expect(201);
+
+      expect(response.body).toEqual({
+        access_token: expect.any(String),
+      });
+    });
   });
 });
